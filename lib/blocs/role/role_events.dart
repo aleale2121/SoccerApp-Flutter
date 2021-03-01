141ad1cc5fc7e32c6e398f7afd2a38a2dea01d18
getch04 <getbet04@gmail.com>
2021-03-01 04:27:28 +0300

    role events in the bloc commited

---
+++ lib/blocs/role/role_events.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import '../../models/user.dart';
+
+abstract class RoleEvents extends Equatable {}
+
+class GetRoleEvent extends RoleEvents {
+  GetRoleEvent();
+  @override
+  List<Object> get props => [];
+}
+
+class PostRoleEvent extends RoleEvents {
+  final Role role;
+  PostRoleEvent({@required this.role});
+  @override
+  List<Object> get props => [];
+}
+
+class UpdateRoleEvent extends RoleEvents {
+  final Role role;
+  UpdateRoleEvent({@required this.role});
+  @override
+  List<Object> get props => [];
+}
+
+class DeleteRoleEvent extends RoleEvents {
+  final String RoleId;
+  DeleteRoleEvent({@required this.RoleId});
+
+  @override
+  List<Object> get props => [];
+}
