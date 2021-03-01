374e746deaadd3aa97b5ef83152217046b5e3d90
johnzdictator <yohannesketemazeleke@gmail.com>
2021-03-01 04:21:17 +0300

    result bloc commit

---
+++ lib/blocs/result/results_event.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import '../../models/result.dart';
+
+abstract class ResultsEvent extends Equatable {}
+
+class GetResultsEvent extends ResultsEvent {
+  GetResultsEvent();
+  @override
+  List<Object> get props => [];
+}
+
+class PostResultEvent extends ResultsEvent {
+  final Result result;
+  PostResultEvent({@required this.result});
+  @override
+  List<Object> get props => [];
+}
+
+class UpdateResultEvent extends ResultsEvent {
+  final Result result;
+  UpdateResultEvent({@required this.result});
+  @override
+  List<Object> get props => [];
+}
+
+class DeleteResultEvent extends ResultsEvent {
+  final String resultId;
+  DeleteResultEvent({@required this.resultId});
+
+  @override
+  List<Object> get props => [];
+}
