eaf8dd7fe33068389d9e0cd585d0ccade1e33fdd
aleale2121 <alefewyimer4@gmail.com>
2021-03-01 05:01:12 +0300

    update result screen

--- lib/screens/user_results_screen.dart
+++ lib/screens/user_results_screen.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../blocs/result/result.dart';
@@ -30,36 +29,3 @@ class UserResultsScreen extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import '../blocs/result/result.dart';
-import '../widgets/result_comp_user.dart';
-import 'splash_screen.dart';
-
-class UserResultsScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<ResultsBloc, ResultStates>(
-      listener: (context, state) {},
-      builder: (_, state) {
-        print(state.toString());
-        if (state is ResultsFetchingState) {
-          return SplashScreen(title: 'Fetching Results');
-        } else if (state is ResultsFetchedState) {
-          final results = state.results;
-
-          return ListView.builder(
-              padding: EdgeInsets.all(8.0),
-              itemCount: results.length,
-              itemBuilder: (_, idx) => ResultComponent(result: results[idx]));
-        } else if (state is ResultsEmptyState) {
-          return SplashScreen(title: 'No Fixtures Found');
-        } else {
-          return SplashScreen(title: 'Failed to load results');
-        }
-      },
-    );
-  }
-}
->>>>>>> f96d9a84e107d197d89bdc93281fa269fe1f014c
