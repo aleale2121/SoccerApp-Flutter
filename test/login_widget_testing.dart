073ac773559f2b6afe0b04d17a09d0cbfc45f6f8
Yuideg <misganewendeg879@gmail.com>
2021-03-01 04:59:52 +0300

    test commit

--- test/login_widget_testing.dart
+++ test/login_widget_testing.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:soccer_pp/screens/login_screen.dart';
+import 'package:soccerApp/screens/login_screen.dart';
 void main(){
-  testWidgets("login screen",(WidgetTester tester) async{
+  testWidgets("login screen testing",(WidgetTester tester) async{
     await tester.pumpWidget(MaterialApp(home: LoginScreen(),));
-    var usernameTextfield=find.byKey(Key("username"));
+    var usernameTextfield=find.byKey(Key("email"));
     var passwordTextfield=find.byKey(Key("password"));
     var login=find.byKey(Key("login"));
     await expect(usernameTextfield,findsOneWidget);
     await tester.enterText(usernameTextfield, "admin@gmail.com");
     await expect(passwordTextfield,findsOneWidget);
-    await tester.enterText(passwordTextfield, "23782378");
+    await tester.enterText(passwordTextfield, "12345678");
     await tester.tap(login);
     await tester.pump(Duration(seconds: 20));
 
