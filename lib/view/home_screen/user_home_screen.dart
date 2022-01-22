4aff740e71316ebd8fed9ce0f07c21f459c04124
aleale2122 <alefew.ymer@jithvar.com>
2022-01-22 23:49:27 +0300

    fix: color

--- lib/view/home_screen/user_home_screen.dart
+++ lib/view/home_screen/user_home_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+
 import 'package:soccer_app/models/model.dart';
+import 'package:soccer_app/util/util.dart';
 import 'package:soccer_app/view/auth_screens/change_password_screen.dart';
 import 'package:soccer_app/view/auth_screens/change_username_screen.dart';
 import 'package:soccer_app/view/auth_screens/user_delete_account.dart';
-import 'package:soccer_app/util/util.dart';
 import 'package:soccer_app/widgets/app_drawer_user.dart';
+
 import '../fixtures_screen/user_fixtures_screen.dart';
 import '../results_screen/user_results_screen.dart';
 
@@ -34,19 +36,21 @@ class _UserHomeState extends State<UserHome>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('home'),
-        bottom: TabBar(
-          controller: controller,
-          tabs: [
-            Tab(
-              text: "Fixtures",
-            ),
-            Tab(
-              text: "Result",
-            )
-          ],
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Colors.orange,
+          size: 30,
+        ), //add this line here
+        title: Text(
+          'Dashboard',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        backgroundColor: Colors.white,
         actions: [
           PopupMenuButton(
               icon: Icon(Icons.more_vert),
@@ -95,9 +99,77 @@ class _UserHomeState extends State<UserHome>
         ],
       ),
       drawer: UserAppDrawer(),
-      body: TabBarView(
-          controller: controller,
-          children: <Widget>[UserFixturesScreen(), UserResultsScreen()]),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 100,
+            child: TabBar(
+              controller: controller,
+              // indicatorColor: Colors.blueAccent,
+              // labelColor: Colors.blueAccent,
+
+              tabs: [
+                Tab(
+                  child: CustomTabBar(
+                    displayText: "MATCHES",
+                  ),
+                ),
+                Tab(
+                  child: CustomTabBar(
+                    displayText: "RESULTS",
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 600,
+              child: TabBarView(
+                controller: controller,
+                children: [
+                  UserFixturesScreen(),
+                  UserResultsScreen(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomTabBar extends StatelessWidget {
+  final String displayText;
+  final double width;
+  final double height;
+  const CustomTabBar({
+    Key? key,
+    required this.displayText,
+    this.width = 300,
+    this.height = 100,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 5.0,
+      child: Container(
+        alignment: Alignment.center,
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Text(
+          displayText,
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
